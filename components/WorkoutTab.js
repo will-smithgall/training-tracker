4e5c4758f3fa4895359dd477d8a49da1d6e45cff
will <was51@pitt.edu>
2024-04-24 23:16:08 -0400

    switching dates display the workout info from that date

--- components/WorkoutTab.js
+++ components/WorkoutTab.js
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Calendar } from "@/components/ui/calendar";
 import ExerciseCard from "./ExerciseCard";
-const { firebaseApp } = require("./FirebaseConfig");
+const { firebaseApp } = require("./FirebaseConfig"); //Starts the firebase app
 const {
     getFirestore,
     collection,
@@ -12,6 +12,7 @@ const {
     limit,
     where,
     getDocs,
+    Timestamp,
 } = require("firebase/firestore");
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
@@ -19,6 +20,8 @@ export default function WorkoutTab() {
     const db = getFirestore();
     const [calendarDate, setCalendarDate] = React.useState(new Date());
     const [workout, setWorkout] = React.useState([]);
+
+    //TODO: add somewhere to put a button to display a form to add a workout (another spot mentioned in todo below)
 
     // Get most recent workout from firestore
     const getMostRecentWorkout = async () => {
@@ -55,6 +58,57 @@ export default function WorkoutTab() {
         getMostRecentWorkout();
     }, []);
 
+    const getWorkoutByDate = async (date) => {
+        //Check for workout at any point in the selected date
+        const q = query(
+            collection(db, "Workouts"),
+            where("Date", ">=", Timestamp.fromDate(date)),
+            where(
+                "Date",
+                "<",
+                Timestamp.fromDate(
+                    new Date(date.getTime() + 24 * 60 * 60 * 1000)
+                )
+            ),
+            limit(1)
+        );
+
+        getDocs(q).then((snapshot) => {
+            snapshot.forEach((document) => {
+                const data = document.data();
+                let exercises = [];
+
+                for (const name in data) {
+                    const content = data[name];
+                    if (name != "Date") {
+                        exercises = [...exercises, { name, content }];
+                    }
+                }
+
+                setWorkout(exercises);
+            });
+
+            if (snapshot.docs.length == 0) {
+                setWorkout([]);
+            }
+        });
+    };
+
+    React.useEffect(() => {
+        if (!calendarDate) {
+            return;
+        }
+        getWorkoutByDate(calendarDate);
+    }, [calendarDate]);
+
+    React.useEffect(() => {
+        if (!workout) {
+            return;
+        }
+
+        console.log(workout);
+    }, [workout]);
+
     return (
         <Card className="p-3">
             <CardHeader>Recent Workouts</CardHeader>
@@ -65,7 +119,7 @@ export default function WorkoutTab() {
                     onSelect={setCalendarDate}
                 />
                 <div className="flex flex-col gap-2 w-full">
-                    {workout != [] &&
+                    {workout.length != 0 ? (
                         workout.map((exercise, index) => {
                             return (
                                 <ExerciseCard
@@ -76,7 +130,10 @@ export default function WorkoutTab() {
                                     sets={exercise.content.sets}
                                 />
                             );
-                        })}
+                        })
+                    ) : (
+                        <p>No workout found for this day</p> //TODO: Add button to log workout on days with nothing recorded
+                    )}
                 </div>
             </CardContent>
         </Card>
