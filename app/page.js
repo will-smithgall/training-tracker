a3366aee7e0e170bf2894e711b90452653219a58
will <was51@pitt.edu>
2024-04-25 21:37:38 -0400

    removed unneeded router

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
-"use client";
-
-import { useRouter } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import WorkoutTab from "@/components/WorkoutTab";
 
 export default function Home() {
-    //add to firestore docs - https://firebase.google.com/docs/firestore/manage-data/add-data
-    const router = useRouter();
-
     return (
         <main className="flex min-h-screen flex-col items-center p-12 md:p-24">
             <Tabs defaultValue="workout" className="w-full min-w-[320px]">
